b10c3b307a9eb1928c12afe4f7edb3dc71e11ba9
Haroun-Moujahed <haroun.moujahed.tu@gmail.com>
2021-03-11 12:41:10 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <iframe
+          title="main-video"
+          width="800"
+          height="500"
+          src="https://www.youtube.com/embed/ffAYByv2pLc"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <h2>2014 WORLD CUP FINAL: Germany 1-0 Argentina</h2>
+        <p style={{ color: "grey" }}>3,352,992 views•Apr 28, 2018</p>
+        <div className="Comments-part">
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+          <div className="Comment">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+              alt="user-img"
+              width="30px"
+              height="30px"
+            />
+            <p>This video is Awesome, Deutschland uber alles</p>
+          </div>
+        </div>
+      </main>
+      <aside>
+        <img
+          src="https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg"
+          alt="suggestion_video"
+          width="200px"
+          height="140px"
+        />
+        <h5>Video suggested in the suggestion list</h5>
+        <img
+          src="https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg"
+          alt="suggestion_video"
+          width="200px"
+          height="140px"
+        />
+        <h5>Video suggested in the suggestion list</h5>
+        <img
+          src="https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg"
+          alt="suggestion_video"
+          width="200px"
+          height="140px"
+        />
+        <h5>Video suggested in the suggestion list</h5>
+        <img
+          src="https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg"
+          alt="suggestion_video"
+          width="200px"
+          height="140px"
+        />
+        <h5>Video suggested in the suggestion list</h5>
+        <img
+          src="https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg"
+          alt="suggestion_video"
+          width="200px"
+          height="140px"
+        />
+        <h5>Video suggested in the suggestion list</h5>
+        <img
+          src="https://i.pcmag.com/imagery/articles/04oP7J3OIykTchX4vhU57vn-28..1569485834.jpg"
+          alt="suggestion_video"
+          width="200px"
+          height="140px"
+        />
+        <h5>Video suggested in the suggestion list</h5>
+      </aside>
     </div>
   );
 }
